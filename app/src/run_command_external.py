b46a548489253c737c4c0cfb51beb0b56e5b3062
Erik M. Galindo <kenshimota@gmail.com>
2026-07-05 21:40:47 -0300

    codecrafters submit [skip ci]

--- app/src/run_command_external.py
+++ app/src/run_command_external.py
@@ -6,12 +6,6 @@ from .type_command import check_pathname_command
 
 def run_command_external(args: List[str]) -> bool:
     try:
-        print(f"Program was passed {len(args)} args (including program name).")
-        print(f"Arg #0 (program name): {args[0]}")
-
-        for i in range(1, len(args)):
-            print(f"Arg #{i}: {args[i]}")
-
         pathname = check_pathname_command(args[0])
         if not pathname:
             return False
