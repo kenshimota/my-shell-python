6bf438b60408bec6efb20749f24ffa119539ae96
Erik M. Galindo <kenshimota@gmail.com>
2026-07-09 21:38:30 -0300

    codecrafters submit [skip ci]

--- app/src/helpers/process_arguments.py
+++ app/src/helpers/process_arguments.py
@@ -1,38 +1,7 @@
 from typing import List
+from shlex import split
 
 
 def process_arguments(s: str) -> List[str]:
-    args = []
-    open = ''
-    tmp = ''
-
     s = s.strip()
-
-    for c in s:
-        if not open and c == ' ':
-            args.append(tmp.strip())
-            tmp = ''
-            continue
-
-        if not open and (c == '\'' or c == '\"'):
-            open = c
-            continue
-
-        if open and c == open:
-            open = ''
-            args.append(tmp.strip())
-            tmp = ''
-            continue
-
-        tmp += c
-
-    if tmp:
-        args.append(tmp)
-
-    res = []
-    for s in args:
-        if not s.strip():
-            continue
-        res.append(s)
-
-    return res
+    return split(s)
